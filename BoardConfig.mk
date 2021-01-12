687bd4d7456a17c2372fd7185eb73e40d0463bd8
Arne Coucheron <arco68@gmail.com>
2021-01-12 05:45:19 +0100

    santoni: Silence clang warnings during kernel compile
    clang-11: warning: argument unused during compilation:
    '-fuse-ld=lld' [-Wunused-command-line-argument]
    
    Change-Id: I66a629f3573e3fad8aa085ea115475f02d60a7ea
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,8 @@ TARGET_KERNEL_CONFIG := mi8937_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/santoni
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
