3adb86f442bcf6551c63115835f07839db6ad226
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-12-30 11:13:10 +0800

    santoni: Export LLVM=1 to Kernel build
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ TARGET_KERNEL_CONFIG := mi8937_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/santoni
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
+TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # ANT
