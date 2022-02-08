a346e374a66fdc2c6d8fbdc704bd69fcf776272c
Ansh <singhansh64321@gmail.com>
2022-02-08 06:56:31 +0000

    ArcaneOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
-# LineageOS (crDroid)
+# ArcaneOS
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_santoni.mk
+    $(LOCAL_DIR)/aosp_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_santoni-userdebug \
-    lineage_santoni-user \
-    lineage_santoni-eng
+    aosp_santoni-userdebug \
+    aosp_santoni-user \
+    aosp_santoni-eng
