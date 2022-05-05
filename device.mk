3c132b9d4ec6997a0e0341ed8fe857acfb251fec
Ansh <singhansh64321@gmail.com>
2022-05-05 07:33:36 +0000

    Revert "santoni: Include Musicolet"
    This reverts commit 58a21359a0cc020acce614689dd49d5478bae8aa.

--- device.mk
+++ device.mk
@@ -526,7 +526,3 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 # Always preopt extracted APKs to prevent extracting out of the APK
 # for gms modules.
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
-
-# Music Player
- PRODUCT_PACKAGES += \
-        Musicolet
