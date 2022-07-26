050faf2f5c779223f2f755a8754027718703fa27
GhostMaster69-dev <rathore6375@gmail.com>
2022-07-26 11:40:16 +0530

    santoni: Override default Android binutils for arm64
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: Ansh <singhansh64321@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-cosmic
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # ANT
