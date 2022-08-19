af4546f5769b151b4ce270b9a7c7f1166d298e6a
Lucchetto <lucchetto.tie@live.com>
2022-08-19 10:48:18 +0200

    santoni: copy prebuilt libhidlbase from vndk v32
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- device.mk
+++ device.mk
@@ -493,6 +493,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
