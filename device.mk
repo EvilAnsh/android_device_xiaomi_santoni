3fdca740baee7216025997755fdc9bb6ddcc1e02
ItoRenz <itorenz@nusantararom.org>
2022-09-02 03:37:40 +0700

    Revert "santoni: switch audio HAL to V7"
    This reverts commit 6f0a13b71d5f8dc81ca15c1b574b702c0471944b.
    
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- device.mk
+++ device.mk
@@ -109,13 +109,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.audio@7.0 \
-    android.hardware.audio@7.0-impl:32 \
-    android.hardware.audio.common@7.0 \
-    android.hardware.audio.common@7.0-util \
+    android.hardware.audio@6.0 \
+    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio.common@6.0 \
+    android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@2.0-impl:32 \
-    android.hardware.audio.effect@7.0 \
-    android.hardware.audio.effect@7.0-impl:32 \
+    android.hardware.audio.effect@6.0 \
+    android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.broadcastradio@1.0-impl
 
 # Audio features
@@ -135,7 +135,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 PRODUCT_COPY_FILES += \
-	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
 	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
 	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
 	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
