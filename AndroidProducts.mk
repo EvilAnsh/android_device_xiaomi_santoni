e35eb17883130b9d61915594211793e2d288dc5a
Ansh <singhansh64321@gmail.com>
2022-09-16 18:00:04 +0000

    santoni: CherishOS Bringup
    Signed-off-by: Ansh <singhansh64321@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
-# LineageOS (crDroid)
+# CherishOS
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_santoni.mk
+    $(LOCAL_DIR)/cherish_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_santoni-userdebug \
-    lineage_santoni-user \
-    lineage_santoni-eng
+    cherish_santoni-userdebug \
+    cherish_santoni-user \
+    cherish_santoni-eng
