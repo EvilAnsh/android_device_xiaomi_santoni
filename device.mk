e9ee967c3fe17759980c0e15247e74b8fe1ed9c4
Ansh <singhansh64321@gmail.com>
2022-09-16 18:52:54 +0000

    santoni: Add Support for android.hardware.power-service HAL
    Signed-off-by: Ansh <singhansh64321@gmail.com>

--- device.mk
+++ device.mk
@@ -382,6 +382,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-service \
     android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor \
     android.hardware.power@1.3.vendor
